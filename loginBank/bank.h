198e89640f008fdbc874bc45c91c13bf354171bd
Claude_Delcroix <166008956+Yurri5code@users.noreply.github.com>
2024-12-14 01:16:30 +0300

    Create bank.h

---
+++ loginBank/bank.h
@@ -0,0 +1,43 @@
+//
+// Created by claude delcroix on 30/11/2024.
+//
+
+#ifndef BANK_H
+#define BANK_H
+#include <stdbool.h>
+int menu() ;
+#define MAX 20
+
+typedef struct account account;
+struct account {
+    char first_name[MAX];
+    char last_name[MAX];
+    char username[MAX];
+    char phone[MAX];
+    char gender;
+    char DOB[MAX];
+    char password[MAX];
+};
+
+void diviser();
+char* initialisation();
+void passWord(account* person);
+char* verifiePassword() ;
+bool isTrue(const char* person,const char* pass);
+account putInformation(account person);
+void clearInputBuffer();
+int menuAfterInformation(account person);
+void printInfo(account person);
+void choiceAfterInformation(const int m,const account person);
+void passWordAndUsernameInFile(char* passWord,char* userName);
+void passwordInFile(char* password);
+void userInFile(char* username);
+char* realPassword();
+char* realUser();
+char* passWordToConnect();
+char* username();
+void writeToFile(account person,const char* filename);
+account readFromFile(account person,const char* filename);
+inline void placeTheEndLine(char* string);
+
+#endif //BANK_H
